70f6b026e5c87fb26e5079cff6092e9ce8a9b3cb
fikrifahruroji <fikrifahruroji@live.com>
2016-12-14 14:16:32 +0700

    Remove progressbar

--- app/src/main/java/id/fikri/testasynctask/MainActivity.java
+++ app/src/main/java/id/fikri/testasynctask/MainActivity.java
@@ -1,15 +1,12 @@
 package id.fikri.testasynctask;
 
-import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 
