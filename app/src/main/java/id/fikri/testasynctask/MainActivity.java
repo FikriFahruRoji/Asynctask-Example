2c4926d49e755c9c12973e028eac395390dc69d6
fikrifahruroji <fikrifahruroji@live.com>
2016-12-14 13:19:11 +0700

    New Project

---
+++ app/src/main/java/id/fikri/testasynctask/MainActivity.java
@@ -0,0 +1,23 @@
+package id.fikri.testasynctask;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
